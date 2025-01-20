aad9ac87d1d6c0cef6226b140f41b6298b9cb6d2
anoduck <9925396+anoduck@users.noreply.github.com>
2025-01-20 03:01:05 -0500

    perf(alwaysdark.js): simplified Code Base
    Greater simplification of Code Base to rely on Hinode's existing functionality.

--- assets/js/modules/alwaysdark/alwaysdark.js
+++ assets/js/modules/alwaysdark/alwaysdark.js
@@ -1,13 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     const theme = 'dark'
-    const darkOn = false
     const storedTheme = () => localStorage.getItem('theme');
     var checkbox = document.querySelector('input.navbar-mode-selector');
 
-    if (checkbox != null) {
-        if (storedTheme() != theme || checkbox.checked != darkOn) {
+    if (checkbox !== null) {
+        while (storedTheme() !== theme) {
             checkbox.click()
-            localStorage.setItem('theme', theme)
+            window.reload()
         }
     }
 });
