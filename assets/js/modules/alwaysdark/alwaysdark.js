12829d479a4cbedb411ce48f73337517828cf765
anoduck <9925396+anoduck@users.noreply.github.com>
2025-05-10 06:41:52 -0400

    fix(alwaysdark.js): redesign of approach
    reduced load, improved speed, and simplified complexity of the module.

--- assets/js/modules/alwaysdark/alwaysdark.js
+++ assets/js/modules/alwaysdark/alwaysdark.js
@@ -2,5 +2,5 @@ document.addEventListener("load", function() {
     const theme = 'dark';
     localStorage.setItem('theme', theme);
     document.documentElement.setAttribute('data-bs-theme', theme);
-    document.getElementById('navbar-mode-checkbox').style.display = 'none';
+    document.getElementsByClassName('mode-switch').style.display = 'none';
 });
